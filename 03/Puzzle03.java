d359218e9da1afbc3b6705b6adda105d50073cca
Eric Nguyen <tuk94307@temple.edu>
2021-12-05 18:48:03 -0500

    Day 3 Part 2

--- 03/Puzzle03.java
+++ 03/Puzzle03.java
@@ -1,62 +1,176 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 class Puzzle03 {
     public static void main(String[] args) {
         File file = new File(args[0]);
-        DiagnosticReport report = DiagnosticsInterpreter.parse(file);
+        DiagnosticsInterpreter interpreter = new DiagnosticsInterpreter(file);
+        DiagnosticReport report = interpreter.getResults();
         System.out.println(report);
         System.out.println("Power consumption: " + report.powerConsumption());
+        System.out.println("Life support rating: " + report.lifeSupportRating());
     }
 }
 
-record DiagnosticReport(int length, int gamma, int epsilon) {
+record DiagnosticReport(int length, int gamma, int epsilon, int oxygenGeneratorRating, int co2ScrubberRating) {
     int powerConsumption() {
         return gamma * epsilon;
+    }
+
+    int lifeSupportRating() {
+        return oxygenGeneratorRating * co2ScrubberRating;
     }
 }
 
 class DiagnosticsInterpreter {
-    public static DiagnosticReport parse(File file) {
+    List<String> report;
+    int codeLength;
+    DiagnosticReport results;
+
+    DiagnosticsInterpreter(File file) {
+        this.report = readReport(file);
+        this.codeLength = calculateCodeLength(file);
+        this.results = interpretReport();
+    }
+
+    public DiagnosticReport getResults() {
+        return this.results;
+    }
+
+    public int getReportLength() {
+        return this.report.size();
+    }
+
+    public int getCodeLength() {
+        return this.codeLength;
+    }
+
+    private static int calculateCodeLength(File file) {
         try {
             Scanner s = new Scanner(file);
             int codeLength = s.nextLine().length();
-            int[] frequencies = new int[codeLength];
-            int reportLength = 0;
             s.close();
-
-            s = new Scanner(file);
-            while (s.hasNextLine()) {
-                Code tokens = new Code(tokenize(s.nextLine()));
-                for (int i = 0; i < codeLength; i++) {
-                    if (tokens.getBit(i)) {
-                        frequencies[i]++;
-                    }
-                }
-                reportLength++;
-            }
-            s.close();
-
-            Code gammaCode = calculateGamma(frequencies, reportLength);
-            Code epsilonCode = gammaCode.inverse();
-            return new DiagnosticReport(reportLength, gammaCode.toDecimal(), epsilonCode.toDecimal());
+            return codeLength;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-            return new DiagnosticReport(-1, 0, 0);
+            return 0;
         }
     }
 
-    private static Code calculateGamma(int[] frequencies, int reportLength) {
-        boolean[] gammaCode = new boolean[frequencies.length];
-        for (int i = 0; i < frequencies.length; i++) {
-            if (frequencies[i] > reportLength / 2) {
+    private DiagnosticReport interpretReport() {
+        Code gammaCode = calculateGamma();
+        Code epsilonCode = gammaCode.inverse();
+        Code oxygenGeneratorRating = calculateOxygenGeneratorRating();
+        Code co2ScrubberRating = calculateCo2ScrubberRating();
+        return new DiagnosticReport(
+            getReportLength(),
+            gammaCode.toDecimal(),
+            epsilonCode.toDecimal(),
+            oxygenGeneratorRating.toDecimal(),
+            co2ScrubberRating.toDecimal()
+        );
+    }
+
+    private List<String> readReport(File file) {
+        try {
+            List<String> list = new LinkedList<>();
+            Scanner s = new Scanner(file);
+            while (s.hasNextLine()) {
+                list.add(s.nextLine());
+            }
+            s.close();
+            return list;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return new LinkedList<String>();
+        }
+    }
+
+    private int[] calculateFrequencies(List<String> report) {
+        int[] frequencies = new int[codeLength];
+        for (String s : report) {
+            Code tokens = new Code(tokenize(s));
+            for (int i = 0; i < codeLength; i++) {
+                if (tokens.getBit(i)) {
+                    frequencies[i]++;
+                }
+            }
+        }
+
+        return frequencies;
+    }
+
+    private Code calculateGamma() {
+        int[] frequencies = calculateFrequencies(this.report);
+        boolean[] gammaCode = new boolean[getCodeLength()];
+        for (int i = 0; i < getCodeLength(); i++) {
+            if (frequencies[i] > getReportLength() / 2) {
                 gammaCode[i] = true;
             } else {
                 gammaCode[i] = false;
             }
         }
         return new Code(gammaCode);
+    }
+
+    private Code calculateOxygenGeneratorRating() {
+        List<String> report = new LinkedList<>(this.report);
+        boolean[] oxygrCode = new boolean[getCodeLength()];
+        for (int i = 0; i < getCodeLength(); i++) {
+            int[] frequencies = calculateFrequencies(report);
+            final int finalI = i;
+            if (report.size() == 1) {
+                for (int j = i; j < getCodeLength(); j++) {
+                    if (report.get(0).charAt(j) == '1') {
+                        oxygrCode[j] = true;
+                    } else {
+                        oxygrCode[j] = false;
+                    }
+                }
+                break;
+            }
+            if ((report.size() % 2 == 0 && frequencies[i] == report.size() / 2)
+                || (frequencies[i] > report.size() / 2)) {
+                oxygrCode[i] = true;
+                report.removeIf(code -> code.charAt(finalI) == '0');
+            } else {
+                oxygrCode[i] = false;
+                report.removeIf(code -> code.charAt(finalI) == '1');
+            }
+        }
+        return new Code(oxygrCode);
+    }
+
+    private Code calculateCo2ScrubberRating() {
+        List<String> report = new LinkedList<>(this.report);
+        boolean[] co2srcode = new boolean[getCodeLength()];
+        for (int i = 0; i < getCodeLength(); i++) {
+            int[] frequencies = calculateFrequencies(report);
+            final int finalI = i;
+            if (report.size() == 1) {
+                for (int j = i; j < getCodeLength(); j++) {
+                    if (report.get(0).charAt(j) == '1') {
+                        co2srcode[j] = true;
+                    } else {
+                        co2srcode[j] = false;
+                    }
+                }
+                break;
+            }
+            if ((report.size() % 2 == 0 && frequencies[i] == report.size() / 2)
+                || (frequencies[i] > report.size() / 2)) {
+                co2srcode[i] = false;
+                report.removeIf(code -> code.charAt(finalI) == '1');
+            } else {
+                co2srcode[i] = true;
+                report.removeIf(code -> code.charAt(finalI) == '0');
+            }
+        }
+        return new Code(co2srcode);
     }
 
     private static boolean[] tokenize(String code) {
