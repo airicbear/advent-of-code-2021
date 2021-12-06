0693a4c9f54824c40bf601ee397e7b0e59d00b0f
Eric Nguyen <tuk94307@temple.edu>
2021-12-06 17:52:12 -0500

    Day 6 Part 2

--- 06/Puzzle06.java
+++ 06/Puzzle06.java
@@ -1,21 +1,21 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 class Puzzle06 {
     public static void main(String[] args) {
-        File file = new File(args[0]);
+        final File file = new File(args[0]);
         try {
-            Scanner s = new Scanner(file);
-            String[] timersInput = s.nextLine().split(",");
-            int[] timers = new int[timersInput.length];
+            final Scanner s = new Scanner(file);
+            final String[] timersInput = s.nextLine().split(",");
+            final int[] timers = new int[timersInput.length];
             for (int i = 0; i < timers.length; i++) {
                 timers[i] = Integer.parseInt(timersInput[i]);
             }
-            School school = new School(timers);
-            school.grow(80);
+            final School school = new School(timers);
+            school.grow(256);
             System.out.println(school.size());
             s.close();
         } catch (FileNotFoundException e) {
@@ -25,27 +25,24 @@ class Puzzle06 {
 }
 
 class School {
-    List<Lanternfish> fish;
-    List<Lanternfish> newFish;
+    final Map<Integer, Long> fish;
 
     School(int[] timers) {
-        this.fish = new ArrayList<>();
-        this.newFish = new ArrayList<>();
+        this.fish = new HashMap<>();
+        for (int i = 0; i <= 8; i++) {
+            this.fish.put(i, 0L);
+        }
         for (int i = 0; i < timers.length; i++) {
-            this.fish.add(new Lanternfish(timers[i]));
+            this.fish.put(timers[i], this.fish.getOrDefault(timers[i], 0L) + 1);
         }
     }
 
-    public void addFish(Lanternfish fish) {
-        this.fish.add(fish);
-    }
-
-    public List<Lanternfish> getFish() {
-        return this.fish;
-    }
-
-    public int size() {
-        return this.fish.size();
+    public long size() {
+        long size = 0;
+        for (long fishCount : this.fish.values()) {
+            size += fishCount;
+        }
+        return size;
     }
 
     public void grow(int numDays) {
@@ -55,57 +52,11 @@ class School {
     }
 
     public void grow() {
-        this.fish.addAll(this.newFish);
-        this.newFish.clear();
-        for (Lanternfish fish : this.fish) {
-            if (fish.isWaiting()) {
-                fish.stopWaiting();
-            } else {
-                fish.tick();
-                if (fish.isZero()) {
-                    newFish.add(new Lanternfish());
-                }
-            }
+        long zeroCount = this.fish.get(0);
+        for (int i = 0; i < this.fish.size() - 1; i++) {
+            this.fish.put(i, this.fish.get(i + 1));
         }
-    }
-}
-
-class Lanternfish {
-    int timer;
-    boolean waiting;
-
-    Lanternfish() {
-        this.timer = 8;
-        this.waiting = true;
-    }
-
-    Lanternfish(int timer) {
-        this.timer = timer;
-        this.waiting = false;
-    }
-
-    boolean isWaiting() {
-        return this.waiting; 
-    }
-
-    void stopWaiting() {
-        this.waiting = false;
-    }
-
-    void tick() {
-        if (timer > 0) {
-            timer--;
-        } else {
-            timer = 6;
-        }
-    }
-
-    boolean isZero() {
-        return this.timer == 0;
-    }
-
-    @Override
-    public String toString() {
-        return Integer.toString(this.timer);
+        this.fish.put(6, this.fish.get(6) + zeroCount);
+        this.fish.put(8, zeroCount);
     }
 }
