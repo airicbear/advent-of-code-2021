3e80fec35c862adb1c6a40728ac9228a19280c65
Eric Nguyen <tuk94307@temple.edu>
2021-12-06 12:32:30 -0500

    Day 5 Part 2

--- 05/Puzzle05.java
+++ 05/Puzzle05.java
@@ -19,16 +19,97 @@ class Puzzle05 {
 }
 
 record Line(Point point1, Point point2) {
-    boolean isVertical() {
-        return point1.x() == point2.x();
+    int angle() {
+        Point dPoint = point1.delta(point2);
+        return (int) Math.round(Math.toDegrees(Math.atan2(dPoint.y(), dPoint.x())));
     }
 
-    boolean isHorizontal() {
-        return point1.y() == point2.y();
+    double slope() {
+        Point delta = point1.delta(point2);
+        return delta.y() / delta.x();
+    }
+
+    List<Point> points() {
+        List<Point> points = new ArrayList<>();
+        Point from = from();
+        Point to = to();
+        Point dPoint = from.delta(to);
+        int[] xs = new int[dPoint.x() + 1];
+        int[] ys = new int[dPoint.x() + 1];
+
+        if (dPoint.x() == 0) {
+            // Vertical line equation
+            xs = new int[dPoint.y() + 1];
+            ys = new int[dPoint.y() + 1];
+            for (int i = 0; i <= dPoint.y(); i++) {
+                int px = from.x();
+                int py = from.y() + i;
+                xs[i] = px;
+                ys[i] = py;
+            }
+        } else {
+            // Line equation
+            xs = new int[dPoint.x() + 1];
+            ys = new int[dPoint.x() + 1];
+            for (int i = 0; i <= dPoint.x(); i++) {
+                int px = from.x() + i;
+                int py = (int) slope() * (px - point1.x()) + point1.y();
+                xs[i] = px;
+                ys[i] = py;
+            }
+        }
+
+        // Draw line
+        for (int i = 0; i < xs.length; i++) {
+            points.add(new Point(xs[i], ys[i]));
+        }
+
+        return points;
+    }
+
+    Point from() {
+        return fromTo()[0];
+    }
+
+    Point to() {
+        return fromTo()[1];
+    }
+
+    private Point[] fromTo() {
+        int fromY, toY, fromX, toX;
+        if (point1().y() < point2().y()) {
+            fromY = point1().y();
+            toY = point2().y();
+        } else {
+            fromY = point2().y();
+            toY = point1().y();
+        }
+        if (point1().x() < point2().x()) {
+            fromX = point1().x();
+            toX = point2().x();
+        } else {
+            fromX = point2().x();
+            toX = point1().x();
+        }
+        Point from = new Point(fromX, fromY);
+        Point to = new Point(toX, toY);
+        return new Point[]{from, to};
     }
 }
 
-record Point(int x, int y) {}
+record Point(int x, int y) {
+    double magnitude() {
+        return Math.sqrt((x * x) + (y * y));
+    }
+
+    int dot(Point other) {
+        return x * other.x() + y * other.y();
+    }
+
+    Point delta(Point other) {
+        return new Point(other.x() - x, other.y() - y);
+    }
+}
 
 class HydrothermalVentScanner {
     private Grid grid;
@@ -41,17 +122,17 @@ class HydrothermalVentScanner {
         try {
             Scanner s = new Scanner(file);
             while (s.hasNextLine()) {
-                String[] pointInputs = s.nextLine().split(" -> ");
-                String[] pointInput1 = pointInputs[0].split(",");
-                String[] pointInput2 = pointInputs[1].split(",");
-                int x1 = Integer.parseInt(pointInput1[0]);
-                int y1 = Integer.parseInt(pointInput1[1]);
-                int x2 = Integer.parseInt(pointInput2[0]);
-                int y2 = Integer.parseInt(pointInput2[1]);
-                Point p1 = new Point(x1, y1);
-                Point p2 = new Point(x2, y2);
-                Line line = new Line(p1, p2);
-                if (line.isHorizontal() || line.isVertical()) {
+                final String[] pointInputs = s.nextLine().split(" -> ");
+                final String[] pointInput1 = pointInputs[0].split(",");
+                final String[] pointInput2 = pointInputs[1].split(",");
+                final int x1 = Integer.parseInt(pointInput1[0]);
+                final int y1 = Integer.parseInt(pointInput1[1]);
+                final int x2 = Integer.parseInt(pointInput2[0]);
+                final int y2 = Integer.parseInt(pointInput2[1]);
+                final Point p1 = new Point(x1, y1);
+                final Point p2 = new Point(x2, y2);
+                final Line line = new Line(p1, p2);
+                if (line.angle() % 45 == 0) {
                     this.grid.markLine(line);
                 }
             }
@@ -70,7 +151,7 @@ class HydrothermalVentScanner {
     }
 
     static class Grid {
-        List<List<Integer>> grid;
+        final List<List<Integer>> grid;
         int xSize = 0;
         int ySize = 0;
 
@@ -93,27 +174,8 @@ class HydrothermalVentScanner {
 
         public void markLine(Line line) {
             updateGrid(line);
-            int fromY, toY, fromX, toX;
-            if (line.point1().y() < line.point2().y()) {
-                fromY = line.point1().y();
-                toY = line.point2().y();
-            } else {
-                fromY = line.point2().y();
-                toY = line.point1().y();
-            }
-            if (line.point1().x() < line.point2().x()) {
-                fromX = line.point1().x();
-                toX = line.point2().x();
-            } else {
-                fromX = line.point2().x();
-                toX = line.point1().x();
-            }
-            Point from = new Point(fromX, fromY);
-            Point to = new Point(toX, toY);
-            for (int y = from.y(); y <= to.y(); y++) {
-                for (int x = from.x(); x <= to.x(); x++) {
-                    this.grid.get(y).set(x, this.grid.get(y).get(x) + 1);
-                }
+            for (Point point : line.points()) {
+                this.grid.get(point.y()).set(point.x(), this.grid.get(point.y()).get(point.x()) + 1);
             }
         }
 
