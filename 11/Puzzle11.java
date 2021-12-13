cc4f582304220bb3bfe41f3dd0ca7b44447feaea
Eric Nguyen <tuk94307@temple.edu>
2021-12-12 20:18:46 -0500

    Day 11 Part 1

---
+++ 11/Puzzle11.java
@@ -0,0 +1,215 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Puzzle11 {
+    public static void main(String[] args) {
+        File file = new File(args[0]);
+        OctopusGrid octopusGrid = OctopusGrid.read(file);
+        System.out.println(octopusGrid);
+        octopusGrid.takeSteps(100);
+        System.out.println(octopusGrid.getNumFlashes());
+    }
+}
+
+class Octopus {
+    private List<Octopus> adjacentOctopi;
+    private int energyLevel;
+    private int row;
+    private int column;
+    private boolean flashed;
+
+    Octopus(int energyLevel, int row, int column) {
+        this.adjacentOctopi = new ArrayList<>();
+        this.energyLevel = energyLevel;
+        this.row = row;
+        this.column = column;
+        this.flashed = false;
+    }
+
+    public boolean isFlashed() {
+        return this.flashed;
+    }
+
+    public void setFlashed(boolean flashed) {
+        this.flashed = flashed;
+    }
+
+    public List<Octopus> getAdjacentOctopi() {
+        return adjacentOctopi;
+    }
+
+    public void setAdjacentOctopi(List<Octopus> adjacentOctopi) {
+        this.adjacentOctopi = adjacentOctopi;
+    }
+
+    public int getEnergyLevel() {
+        return energyLevel;
+    }
+
+    public void setEnergyLevel(int energyLevel) {
+        this.energyLevel = energyLevel;
+    }
+
+    public int getRow() {
+        return row;
+    }
+    
+    public int getColumn() {
+        return column;
+    }
+
+    public void increaseEnergyLevel() {
+        this.energyLevel++;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Octopus(energyLevel=");
+        sb.append(this.energyLevel);
+        sb.append(",row=");
+        sb.append(this.row);
+        sb.append(",column=");
+        sb.append(this.column);
+        sb.append(",flashed=");
+        sb.append(this.flashed);
+        sb.append(")");
+        return sb.toString();
+    }
+}
+
+class OctopusGrid {
+    List<List<Octopus>> octopi;
+    int numFlashes;
+    
+    OctopusGrid(List<List<Octopus>> octopi) {
+        this.octopi = octopi;
+        this.numFlashes = 0;
+        for (List<Octopus> row : octopi) {
+            for (Octopus octopus : row) {
+                calculateAdjacentOctopi(octopus);
+            }
+        }
+    }
+
+    public Octopus getOctopus(int row, int col) {
+        return this.octopi.get(row).get(col);
+    }
+
+    public int getNumFlashes() {
+        return this.numFlashes;
+    }
+
+    public static OctopusGrid read(File file) {
+        try {
+            Scanner s = new Scanner(file);
+            List<List<Octopus>> octopi = new ArrayList<>();
+            int row = 0;
+            while (s.hasNextLine()) {
+                String[] rowInput = s.nextLine().split("");
+                List<Octopus> octopusRow = new ArrayList<>();
+                for (int col = 0; col < rowInput.length; col++) {
+                    int energy = Integer.parseInt(rowInput[col]);
+                    octopusRow.add(new Octopus(energy, row, col));
+                }
+                octopi.add(octopusRow);
+                row++;
+            }
+            s.close();
+
+            return new OctopusGrid(octopi);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void takeSteps(int n) {
+        for (int i = 0; i < n; i++) {
+            takeStep();
+        }
+    }
+
+    public void takeStep() {
+        resetFlash();
+        increaseAll();
+        flashOctopi();
+        setFlashedToZero();
+    }
+
+    private void setFlashedToZero() {
+        for (int row = 0; row < this.octopi.size(); row++) {
+            for (int col = 0; col < this.octopi.get(0).size(); col++) {
+                if (getOctopus(row, col).isFlashed()) {
+                    getOctopus(row, col).setEnergyLevel(0);
+                }
+            }
+        }
+    }
+
+    private void flashOctopi() {
+        for (int row = 0; row < this.octopi.size(); row++) {
+            for (int col = 0; col < this.octopi.get(0).size(); col++) {
+                if (getOctopus(row, col).getEnergyLevel() > 9) {
+                    flash(getOctopus(row, col));
+                }
+            }
+        }
+    }
+
+    private void increaseAll() {
+        for (int row = 0; row < this.octopi.size(); row++) {
+            for (int col = 0; col < this.octopi.get(0).size(); col++) {
+                getOctopus(row, col).increaseEnergyLevel();
+            }
+        }
+    }
+
+    private void resetFlash() {
+        for (int row = 0; row < this.octopi.size(); row++) {
+            for (int col = 0; col < this.octopi.get(0).size(); col++) {
+                getOctopus(row, col).setFlashed(false);
+            }
+        }
+    }
+
+    private void flash(Octopus octopus) {
+        this.numFlashes++;
+        octopus.setEnergyLevel(0);
+        octopus.setFlashed(true);
+        for (Octopus adjOctopus : octopus.getAdjacentOctopi()) {
+            adjOctopus.increaseEnergyLevel();
+            if (adjOctopus.getEnergyLevel() > 9 && !adjOctopus.isFlashed()) {
+                adjOctopus.setEnergyLevel(0);
+                flash(adjOctopus);
+            }
+        }
+    }
+
+    private void calculateAdjacentOctopi(Octopus octopus) {
+        List<Octopus> adjacentOctopi = new ArrayList<>();
+        for (int row = octopus.getRow() - 1; row <= octopus.getRow() + 1; row++) {
+            for (int col = octopus.getColumn() - 1; col <= octopus.getColumn() + 1; col++) {
+                if (row >= 0 && row < octopi.size() && col >= 0 && col < octopi.get(0).size() && !(row == octopus.getRow() && col == octopus.getColumn())) {
+                    adjacentOctopi.add(getOctopus(row, col));
+                }
+            }
+        }
+        octopus.setAdjacentOctopi(adjacentOctopi);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (List<Octopus> row : octopi) {
+            for (Octopus octopus : row) {
+                sb.append(octopus.getEnergyLevel());
+            }
+            sb.append('\n');
+        }
+        return sb.toString();
+    }
+}
